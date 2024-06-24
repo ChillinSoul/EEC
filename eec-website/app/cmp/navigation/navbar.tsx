2015d3093f7c9373e84b5179592c1cc041e90755
A. Bergiers <99248410+ChillinSoul@users.noreply.github.com>
2024-06-24 14:35:46 +0200

    z fighting in the nav fixed

--- eec-website/app/cmp/navigation/navbar.tsx
+++ eec-website/app/cmp/navigation/navbar.tsx
@@ -12,7 +12,7 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ navItems }) => {
     return (
-        <nav className="fixed  w-full ">
+        <nav className="fixed  w-full">
             <div className="flex flex-row  border-line w-full justify-between p-5">
                 <img src="/eec-logo.png" alt="eec logo" className="w-10 h-10" />
                 <ul className="flex flex-row gap-10 m-auto">
