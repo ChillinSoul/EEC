47a572d3110ec7f78a37e7cce5b682f67c966da0
A. Bergiers <99248410+ChillinSoul@users.noreply.github.com>
2024-06-24 14:48:22 +0200

    navbar css improvements

--- eec-website/app/cmp/navigation/navbar.tsx
+++ eec-website/app/cmp/navigation/navbar.tsx
@@ -14,7 +14,7 @@ const Navbar: React.FC<NavbarProps> = ({ navItems }) => {
     return (
         <nav className="fixed  w-full">
             <div className="flex flex-row  border-line w-full justify-between p-5">
-                <img src="/eec-logo.png" alt="eec logo" className="w-10 h-10" />
+                <img src="/eec-logo.png" alt="eec logo" className="w-10 h-11" />
                 <ul className="flex flex-row gap-10 m-auto">
                     {navItems.map((item, index) => (
                         <Navlink key={index} text={item.text} link={item.link} />
