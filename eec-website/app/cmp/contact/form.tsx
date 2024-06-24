4cc91383d0e3b1e97e598ef4fe3e961c5e3a610b
A. Bergiers <99248410+ChillinSoul@users.noreply.github.com>
2024-06-24 17:25:42 +0200

    added toast notifications to the contact form

--- eec-website/app/cmp/contact/form.tsx
+++ eec-website/app/cmp/contact/form.tsx
@@ -1,11 +1,17 @@
 "use client";
+
+import React from "react";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import FormLine from "./formLine";
 import "./styles.css";
+
 export interface FormItem {
   label: string;
   type: string;
   placeholder: string;
 }
+
 interface ContactFormProps {
   formItems: FormItem[];
   job?: string;
@@ -20,15 +26,18 @@ const ContactForm: React.FC<ContactFormProps> = ({ formItems, title, job }) => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
     console.log("Form submitted");
-    if (job){
-        console.log("Job: ", job);
+    toast.success("Form submitted successfully!");
+    if (job) {
+      console.log("Job: ", job);
+      toast.info(`message sent for job: ${job}`);
     }
     console.log(data);
   };
 
   return (
     <div className="flex flex-col gap-5 m-auto">
-    <h2 className="text-2xl text-[var(--color)]">{title}</h2>
+      <ToastContainer />
+      <h2 className="text-2xl text-[var(--color)]">{title}</h2>
       <form onSubmit={onsubmit}>
         {formItems.map((item, index) => (
           <FormLine key={index} item={item} />
@@ -38,4 +47,5 @@ const ContactForm: React.FC<ContactFormProps> = ({ formItems, title, job }) => {
     </div>
   );
 };
+
 export default ContactForm;
