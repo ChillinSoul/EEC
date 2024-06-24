6a925a7b48209104efa42f7dcbe9db374b3c8ae4
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2024-06-24 14:49:14 +0200

    chore: Update hero section and navigation styles

--- eec-website/app/cmp/hero/heroSection.tsx
+++ eec-website/app/cmp/hero/heroSection.tsx
@@ -1,34 +1,28 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="hero-section w-full text-center relative">
-      <div className="bg-gray-100 dark:bg-gray-800 py-20 relative z-10">
+    <section className="relative w-full text-center hero-section">
+      <img
+        src="/hero_image.webp"
+        alt="Hero Image"
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      />
+      <div className="relative z-10 flex flex-col items-center justify-center h-full py-20 bg-black bg-opacity-50">
         <div className="container mx-auto">
-          <h1 className="text-4xl font-bold mb-4 text-white">
+          <h1 className="mb-4 text-4xl font-bold text-white">
             ECAM Engineering Consult
           </h1>
-          <p className="text-xl mb-8 text-white">
+          <p className="mb-8 text-xl text-white">
             The bridge between academic and professional life
           </p>
           <Link href="/about">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button className="px-4 py-2 font-bold text-black bg-white rounded hover:bg-gray-200">
               Learn More
             </button>
           </Link>
         </div>
-      </div>
-      <div className="absolute top-0 left-0 w-full h-full z-0">
-        <Image
-          src="/hero_image.webp"
-          alt="Hero Image"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-          className="opacity-70"
-        />
       </div>
     </section>
   );
